9de93d15e3ae8c674ed46cffdb6a02104942a867
Teuku Hashrul <hasrultk98@gmail.com>
2021-08-08 23:19:52 +0700

    fixed conflicts

--- index.js
+++ index.js
@@ -935,19 +935,6 @@ app.get('/form', authenticateJWT, async (req, res) => {
         })
     })
 })
-function parseJwt (token) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-
-    return JSON.parse(jsonPayload);
-};
-function getUserID (req){
-    var payload = parseJwt(req.headers.authorization)
-    return payload.id;
-}
 
 app.get('/test',authenticateJWT,async(req,res) => {
     getUserID(req)
